0fda09f876428f47947be53a3ffd7d168ceaf8fc
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-06 13:50:55 +0900

    feat(InputView): 출발역 입력 기능 추가
    docs: 출발역 입력 완료 체크

--- src/main/java/subway/InputView.java
+++ src/main/java/subway/InputView.java
@@ -4,9 +4,15 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_SELECT_MENU = "## 원하는 기능을 선택하세요.\n";
+    private static final String INPUT_START_STATION = "## 출발역을 입력하세요.\n";
 
     public String readSelectMenu(Scanner scanner) {
         System.out.println(INPUT_SELECT_MENU);
+        return scanner.nextLine();
+    }
+
+    public String readStartStation(Scanner scanner) {
+        System.out.println(INPUT_START_STATION);
         return scanner.nextLine();
     }
 }
