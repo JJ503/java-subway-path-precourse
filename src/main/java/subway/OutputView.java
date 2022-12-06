be670cf696e38b9d880d0fe45b99cc7f9ecdc1a7
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-06 17:20:48 +0900

    feat(OutputView): 조회 결과 출력 기능 추가
    docs: 조회 결과 출력 완료 체크

--- src/main/java/subway/OutputView.java
+++ src/main/java/subway/OutputView.java
@@ -2,9 +2,19 @@ package subway;
 
 import jdk.jfr.internal.tool.Main;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class OutputView {
     private static final String MAIN_MENU = "## 메인 화면";
     private static final String PATH_MENU = "## 경로 기준";
+    private static final String RESULT = "## 조회 결과";
+    private static final String INFO_TAG = "[INFO] %s\n";
+    private static final String DIVIDE_LINE = "---";
+    private static final String TOTAL_DISTANCE = "총 거리: %dkm";
+    private static final String TOTAL_TIME = "총 소요 시간: %d분";
 
     public void printMainMenu() {
         System.out.println(MAIN_MENU);
@@ -18,5 +28,21 @@ public class OutputView {
         for (PathMenu pathMenu : PathMenu.values()) {
             System.out.println(pathMenu.getMenuMessage());
         }
+    }
+
+    public void printPathResult(List<String> path, int distance, int time) {
+        List<String> values = weightToStringFormat(distance, time);
+        values = Stream.concat(values.stream(), path.stream())
+                .collect(Collectors.toList());
+
+        System.out.println(RESULT);
+        for (String value : values) {
+            System.out.printf(INFO_TAG, value);
+        }
+    }
+
+    private List<String> weightToStringFormat(int distance, int time) {
+        return Arrays.asList(DIVIDE_LINE, String.format(TOTAL_DISTANCE, distance),
+                String.format(TOTAL_TIME, time), DIVIDE_LINE);
     }
 }
