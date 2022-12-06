886ac22614079dfeca3c495b72e92f9abdf202d4
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-06 17:45:49 +0900

    style(InputView): 개행문자 추가

--- src/main/java/subway/InputView.java
+++ src/main/java/subway/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
     private static final String INPUT_SELECT_MENU = "\n## 원하는 기능을 선택하세요.";
     private static final String INPUT_START_STATION = "\n## 출발역을 입력하세요.";
-    private static final String INPUT_ARRIVAL_STATION = "\n## 출발역을 입력하세요.";
+    private static final String INPUT_ARRIVAL_STATION = "\n## 도착역을 입력하세요.";
 
     public String readSelectMenu(Scanner scanner) {
         System.out.println(INPUT_SELECT_MENU);
