fe22911e89f245572f55803d5913d0c52c296920
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-06 14:08:13 +0900

    refactor(PathMenu): 경로 메뉴 목록 문자열 상수를 enum으로 생성

---
+++ src/main/java/subway/PathMenu.java
@@ -0,0 +1,28 @@
+package subway;
+
+import java.util.Arrays;
+
+public enum PathMenu {
+    PATH_MENU1("1", "최단 거리"),
+    PATH_MENU2("2", "최소 시간"),
+    PATH_MENU3("B", "돌아가기");
+
+    private final String option;
+    private final String menu;
+
+    PathMenu(String option, String menu) {
+        this.option = option;
+        this.menu = menu;
+    }
+
+    public String getMenuMessage() {
+        return String.format("%s. %s\n", option, menu);
+    }
+
+    public static PathMenu getMenu(String option) {
+        return Arrays.stream(values())
+                .filter(value -> value.option.equals(option))
+                .findAny()
+                .orElse(null);
+    }
+}
