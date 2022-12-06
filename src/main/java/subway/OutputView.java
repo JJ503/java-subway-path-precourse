736bc13d4493f2d60cd14e4269afb3b65c0839b5
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-06 13:46:02 +0900

    feat(OutputView): 경로 기준 출력 기능 추가
    docs: 경로 기준 출력 완료 체크

--- src/main/java/subway/OutputView.java
+++ src/main/java/subway/OutputView.java
@@ -2,8 +2,13 @@ package subway;
 
 public class OutputView {
     private static final String MAIN_MENU = "## 메인 화면\n1. 경로 조회\nQ. 종료\n";
+    private static final String PATH_MENU = "## 경로 기준\n1. 최단 거리\n2. 최소 시간\nB. 돌아가기\n";
 
     public void printMainMenu() {
         System.out.println(MAIN_MENU);
+    }
+
+    public void printPathMenu() {
+        System.out.println(PATH_MENU);
     }
 }
