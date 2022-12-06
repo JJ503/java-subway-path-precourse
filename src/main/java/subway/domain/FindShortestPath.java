4125d2304c5817e808f2d39fa23078fc088a2794
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-06 17:41:27 +0900

    style(FindShortestPath): 변수 및 함수 순서 변경
    time과 distance에 관련된 내용의 순서를 바꾸었다.

--- src/main/java/subway/domain/FindShortestPath.java
+++ src/main/java/subway/domain/FindShortestPath.java
@@ -7,29 +7,26 @@ import org.jgrapht.graph.WeightedMultigraph;
 import java.util.List;
 
 public class FindShortestPath {
-    WeightedMultigraph<String, DefaultWeightedEdge> timeGraph = new WeightedMultigraph(DefaultWeightedEdge.class);
     WeightedMultigraph<String, DefaultWeightedEdge> distanceGraph = new WeightedMultigraph(DefaultWeightedEdge.class);
+    WeightedMultigraph<String, DefaultWeightedEdge> timeGraph = new WeightedMultigraph(DefaultWeightedEdge.class);
 
     public void addVertex(String station) {
-        timeGraph.addVertex(station);
         distanceGraph.addVertex(station);
+        timeGraph.addVertex(station);
     }
 
-    public void addTimeEdgeWeight(String station1, String station2, int timeWeight) {
-        timeGraph.setEdgeWeight(timeGraph.addEdge(station1, station2), timeWeight);
-    }
-
-    public void addDistanceEdgeWeight(String station1, String station2, int distanceWeight) {
+    public void addEdgeWeight(String station1, String station2, int distanceWeight, int timeWeight) {
         distanceGraph.setEdgeWeight(distanceGraph.addEdge(station1, station2), distanceWeight);
-    }
-
-    public List<String> getShortestPathByTime(String station1, String station2) {
-        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(timeGraph);
-        return dijkstraShortestPath.getPath(station1, station2).getVertexList();
+        timeGraph.setEdgeWeight(timeGraph.addEdge(station1, station2), timeWeight);
     }
 
     public List<String> getShortestPathByDistance(String station1, String station2) {
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(distanceGraph);
+        return dijkstraShortestPath.getPath(station1, station2).getVertexList();
+    }
+
+    public List<String> getShortestPathByTime(String station1, String station2) {
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(timeGraph);
         return dijkstraShortestPath.getPath(station1, station2).getVertexList();
     }
 }
