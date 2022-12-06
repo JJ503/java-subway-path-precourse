2fa83988af0b4109cfffcf91b843b6a70daa9bf3
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-06 17:48:27 +0900

    feat(SectionRepository): 에러 메시지 문자열 상수 추가

--- src/main/java/subway/ExceptionMessage.java
+++ src/main/java/subway/ExceptionMessage.java
@@ -3,7 +3,10 @@ package subway;
 public enum ExceptionMessage {
     INPUT_NONE("값을 입력해 주세요."),
     INPUT_WRONG_MAIN_MENU("존재하지 않는 메뉴입니다. (1 혹은 Q를 입력해 주세요.)"),
-    INPUT_WRONG_PATH_MENU("존재하지 않는 메뉴입니다. (1, 2 혹은 B를 입력해 주세요.)");
+    INPUT_WRONG_PATH_MENU("존재하지 않는 메뉴입니다. (1, 2 혹은 B를 입력해 주세요.)"),
+    INPUT_NOT_EXIST_STATION("존재하지 않는 역입니다."),
+    INPUT_SAME_STATION("출발역과 도착역이 동일합니다."),
+    NO_RESULT_CAN_BE_FOUND("결과를 찾을 수 없습니다.");
 
     private final String exceptionMessage;
     private static final String ERROR_TAG = "[ERROR] ";
