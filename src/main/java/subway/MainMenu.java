222109eb9db5cfa8f31a9fe1170f24d041671458
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-06 14:08:03 +0900

    refactor(MainMenu): 메뉴 목록 문자열 상수를 enum으로 생성

---
+++ src/main/java/subway/MainMenu.java
@@ -0,0 +1,27 @@
+package subway;
+
+import java.util.Arrays;
+
+public enum MainMenu {
+    MAIN_MENU1("1", "경로 조회"),
+    MAIN_MENU2("Q", "종료");
+
+    private final String option;
+    private final String menu;
+
+    MainMenu(String option, String menu) {
+        this.option = option;
+        this.menu = menu;
+    }
+
+    public String getMenuMessage() {
+        return String.format("%s. %s\n", option, menu);
+    }
+
+    public static MainMenu getMenu(String option) {
+        return Arrays.stream(values())
+                .filter(value -> value.option.equals(option))
+                .findAny()
+                .orElse(null);
+    }
+}
