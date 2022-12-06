0e047f7ab898afd473fcbb67f6e9ed3a80e1f767
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-06 16:11:09 +0900

    feat(SubwayInformation): 지하철 노선 및 역 정보를 담은 enum 추가

---
+++ src/main/java/subway/SubwayInformation.java
@@ -0,0 +1,39 @@
+package subway;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum SubwayInformation {
+    LINE1("2호선", Arrays.asList("교대역", "강남역", "역삼역"), Arrays.asList(2, 2), Arrays.asList(3, 3)),
+    LINE2("3호선", Arrays.asList("교대역", "남부터미널역", "양재역", "매봉역"), Arrays.asList(3, 6, 1),
+            Arrays.asList(2, 5, 1)),
+    LINE3("3호선", Arrays.asList("강남역", "양재역", "양재시민의숲역"), Arrays.asList(2, 10), Arrays.asList(8, 3));
+
+    private final String line;
+    private final List<String> stations;
+    private final List<Integer> times;
+    private final List<Integer> distances;
+
+    SubwayInformation(String line, List<String> stations, List<Integer> times, List<Integer> distances) {
+        this.line = line;
+        this.stations = stations;
+        this.times = times;
+        this.distances = distances;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public List<String> getStations() {
+        return stations;
+    }
+
+    public List<Integer> getTimes() {
+        return times;
+    }
+
+    public List<Integer> getDistances() {
+        return distances;
+    }
+}
